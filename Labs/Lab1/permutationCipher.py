aad3eb8c964621d991b4f3e7f9053f1efc602adf
Shone Renjan <shonerenjan@Shones-MacBook-Air-3.local>
2026-09-23 18:15:52 -0500

    debuggingP1

--- Labs/Lab1/permutationCipher.py
+++ Labs/Lab1/permutationCipher.py
@@ -1,2 +1,8 @@
-def permutationCipher(plaintext, key):
-    
+def permutationCipher(cipherText, key):
+    key_length = len(key)
+    for index, letter in enumerate(cipherText):
+        for key_length in letter:
+            print(key_length)
+        
+
+permutationCipher("Q9fL3XvT8pR2mN7kD1sA6cH0yZ5uJ4eBqWn","7145236")
